6427c481ff1b81a0541465c4f8fbea0eb355deb6
attamakevin <kevinejikez@gmail.com>
2023-10-31 23:33:00 +0000

    done

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 """A simple flask app
 """
+
+
+
 from flask import Flask, render_template
 app = Flask(__name__) 
 
+
 @app.route("/")
 def hello_world():
+
     """_say hellow world_
     """
+
     return render_template("0-index.html")
 
 if __name__=="__main__":
