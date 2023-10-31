ecf181c7e6baa2f27b2692de3ac055fd74eb45e5
attamakevin <kevinejikez@gmail.com>
2023-10-31 23:26:39 +0000

    done

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    """say hellow world"""
+    """_say hellow world_
+    """
     return render_template("0-index.html")
 
 if __name__=="__main__":
